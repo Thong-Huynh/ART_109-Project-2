aaf1823f1833d137320b58f3b6d90814a48f09fc
Thong-Huynh <minhthong.huynh@sjsu.edu>
2022-10-31 01:59:39 -0700

    add turtles

---
+++ turtle.js
@@ -0,0 +1,30 @@
+let turtle1 = anime({
+    targets: '.turtle-1',
+    translateY: [-900, 150],
+    duration: 5000,
+    // easing: 'easeInOutSine',
+    easing: 'steps(12)',
+    loop: true,
+});
+
+let turtle2 = anime({
+    targets: '.turtle-2',
+    translateY: [-900, 150],
+    // delay: 200,
+    // opacity: ,
+    duration: 5500,
+    // easing: 'easeInOutSine',
+    easing: 'steps(15)',
+    loop: true,
+});
+
+let turtle3 = anime({
+    targets: '.turtle-3',
+    translateY: [-900, 150],
+    // delay: 200,
+    // opacity: ,
+    duration: 4500,
+    // easing: 'easeInOutSine',
+    easing: 'steps(8)',
+    loop: true,
+});
