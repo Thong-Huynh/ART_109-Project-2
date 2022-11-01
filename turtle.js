dc41423a5f994b01e343d056d12e56d5827b1962
Thong-Huynh <minhthong.huynh@sjsu.edu>
2022-10-31 23:00:04 -0700

    adjust the turtle's path

--- turtle.js
+++ turtle.js
@@ -1,30 +1,27 @@
 let turtle1 = anime({
     targets: '.turtle-1',
-    translateY: [-900, 150],
-    duration: 5000,
-    // easing: 'easeInOutSine',
+    translateY: [0, 1000],
+    translateX: [15, -20, 5],
+    duration: 4500,
     easing: 'steps(12)',
     loop: true,
 });
 
 let turtle2 = anime({
     targets: '.turtle-2',
-    translateY: [-900, 150],
-    // delay: 200,
-    // opacity: ,
-    duration: 5500,
-    // easing: 'easeInOutSine',
-    easing: 'steps(15)',
+    translateY: [0, 1000],
+    translateX: [-10, 15, 0],
+    duration: 4000,
+    easing: 'steps(9)',
     loop: true,
 });
 
 let turtle3 = anime({
     targets: '.turtle-3',
-    translateY: [-900, 150],
-    // delay: 200,
-    // opacity: ,
-    duration: 4500,
+    translateY: [0, 1000],
+    translateX: [5, -20, 15],
+    duration: 5000,
     // easing: 'easeInOutSine',
-    easing: 'steps(8)',
+    easing: 'steps(15)',
     loop: true,
 });
